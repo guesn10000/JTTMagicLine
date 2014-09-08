960fe12748b693df1beea73875a9ea038a05528d
Jymn_Chen <guesn10000@126.com>
2014-09-08 20:06:41 +0800

    Add TODO.h

---
+++ JTTMagicLine/TODO.h
@@ -0,0 +1,18 @@
+//
+//  TODO.h
+//  JTTMagicLine
+//
+//  Created by Jymn_Chen on 14-9-8.
+//  Copyright (c) 2014年 Jymn_Chen. All rights reserved.
+//
+
+#ifndef JTTMagicLine_TODO_h
+#define JTTMagicLine_TODO_h
+
+/*
+ 1.增加设置面板，可以设置替换的字符串和触发替换事件的字符串，关闭该插件的功能等
+ 2.修复Command + Z无法恢复某行字符串的问题，参考VVDocumenter
+ 3.增加对Xcode 6的支持
+ */
+
+#endif
